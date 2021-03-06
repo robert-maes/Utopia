445b9340d8b53bae6a9cffbea1b9d55714e252ce
Rob Maes <robert.maes@smoothstack.com>
2021-03-06 10:40:47 -0600

    migrate to Spring

---
+++ api/src/test/java/com/smoothstack/utopia/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.utopia.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
